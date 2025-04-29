201fdf5478ee54030c9bf92be3b6e6039e522f04
Rendyzet <107420488+Rendyzetx@users.noreply.github.com>
2025-04-29 23:28:39 +0800

    perubahan

--- tes.py
+++ tes.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-# Pastikan kode utama berada di dalam blok 'if __name__ == "__main__":'
+
 if __name__ == '__main__':
     # Load the trained model (gunakan model yang sudah dilatih, misalnya best.pt)
     model = YOLO("runs/train/exp_uang/weights/best.pt")  # Ganti dengan path ke model yang sudah dilatih
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     results = model("tes3.jpg")  # Ganti dengan path ke gambar yang ingin diuji
 
     # Tampilkan hasil deteksi
-    results[0].show()  # Menampilkan gambar dengan bounding box
+    results[0].show()  
 
-    # Jika kamu ingin menyimpan hasil gambar dengan deteksi
-    results[0].save()  # Menyimpan gambar hasil inferensi
+    # menyimpan hasil gambar deteksi
+    results[0].save()  
