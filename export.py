93787aa25f448c56cf3ec7c8a22e47e88df732de
Rendyzet <107420488+Rendyzetx@users.noreply.github.com>
2025-05-20 01:30:23 +0800

    Update export.py

--- export.py
+++ export.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
-# Pastikan kode utama berada di dalam blok 'if __name__ == "__main__":'
+
 if __name__ == '__main__':
-    # Load model yang sudah dilatih (gunakan path ke model yang sudah dilatih, misalnya best.pt)
-    model = YOLO("runs/train/exp_uang/weights/best.pt")  # Ganti dengan path ke model yang sudah dilatih
+    # Load model 
+    model = YOLO("runs/train/exp_uang/weights/best.pt")  
 
     # Export model ke format ONNX
-    model.export(format="onnx")  # Format bisa diubah sesuai kebutuhan (misalnya 'onnx', 'tflite', dll.)
+    model.export(format="onnx")  
 
     print("Model diekspor ke format ONNX.")
